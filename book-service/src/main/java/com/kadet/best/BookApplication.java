b58d786e2635280979aeb5b396f774c702a9c0d4
AlexSoroka <AlexSoroka@coherentsolutions.com>
2017-05-13 02:07:37 +0300

    BEST-6 Add eureka registry and spring cloud config

---
+++ book-service/src/main/java/com/kadet/best/BookApplication.java
@@ -0,0 +1,15 @@
+package com.kadet.best;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BookApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookApplication.class, args);
+  }
+
+}
